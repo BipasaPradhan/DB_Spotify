05fa876de6c7b366b45a0b7a358dd95e191fc9b6
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-07-06 23:17:54 +0700

    added script for adding artist to table

--- init_tables.sql
+++ init_tables.sql
@@ -117,3 +117,17 @@ CREATE TABLE song_stream_counts (
 
     FOREIGN KEY (song_id) REFERENCES songs(song_id) ON DELETE CASCADE
 );
+
+-- Script for inserting artist
+
+INSERT INTO artists (artist_id, name, bio, image_url) VALUES
+      (1, 'Taylor Swift', 'American singer-songwriter known for narrative songwriting.', 'https://fakecdn.com/artist/taylor_swift.jpg?token=fake123abc&expires=1725470000'),
+      (2, 'Drake', 'Canadian rapper, singer, and songwriter.', 'https://fakecdn.com/artist/drake.jpg?token=fake456def&expires=1725470000'),
+      (3, 'BTS', 'South Korean boy band with a global fanbase.', 'https://fakecdn.com/artist/bts.jpg?token=fake789ghi&expires=1725470000'),
+      (4, 'Billie Eilish', 'American singer known for her distinctive style.', 'https://fakecdn.com/artist/billie_eilish.jpg?token=fake101jkl&expires=1725470000'),
+      (5, 'The Weeknd', 'Canadian artist blending R&B and pop.', 'https://fakecdn.com/artist/the_weeknd.jpg?token=fake202mno&expires=1725470000'),
+      (6, 'Ariana Grande', 'Pop and R&B singer known for her vocal range.', 'https://fakecdn.com/artist/ariana_grande.jpg?token=fake303pqr&expires=1725470000'),
+      (7, 'Ed Sheeran', 'English singer-songwriter with global hits.', 'https://fakecdn.com/artist/ed_sheeran.jpg?token=fake404stu&expires=1725470000'),
+      (8, 'Dua Lipa', 'British pop singer and fashion icon.', 'https://fakecdn.com/artist/dua_lipa.jpg?token=fake505vwx&expires=1725470000'),
+      (9, 'Bad Bunny', 'Puerto Rican artist known for Latin trap and reggaeton.', 'https://fakecdn.com/artist/bad_bunny.jpg?token=fake606yz0&expires=1725470000'),
+      (10, 'Olivia Rodrigo', 'American pop-rock artist and actress.', 'https://fakecdn.com/artist/olivia_rodrigo.jpg?token=fake707abc&expires=1725470000');
