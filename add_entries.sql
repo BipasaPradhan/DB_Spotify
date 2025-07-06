469e053e8a9503fa8d9cf867b8d3cfe9864852f8
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-07-07 00:49:50 +0700

    added script for adding genre and album sorry didn't real file name

--- add_entries.sql
+++ add_entries.sql
@@ -11,3 +11,26 @@ INSERT INTO artists (artist_id, name, bio, image_url) VALUES
       (8, 'Dua Lipa', 'British pop singer and fashion icon.', 'https://fakecdn.com/artist/dua_lipa.jpg?token=fake505vwx&expires=1725470000'),
       (9, 'Bad Bunny', 'Puerto Rican artist known for Latin trap and reggaeton.', 'https://fakecdn.com/artist/bad_bunny.jpg?token=fake606yz0&expires=1725470000'),
       (10, 'Olivia Rodrigo', 'American pop-rock artist and actress.', 'https://fakecdn.com/artist/olivia_rodrigo.jpg?token=fake707abc&expires=1725470000');
+
+-- Script for genre
+
+INSERT INTO genres (genre_id, name) VALUES
+                                        (1, 'Pop'),
+                                        (2, 'Hip Hop'),
+                                        (3, 'R&B'),
+                                        (4, 'K-Pop'),
+                                        (5, 'Latin');
+
+-- Script for album
+
+INSERT INTO albums (album_id, artist_id, title, release_date, cover_url) VALUES
+                                                                             (1, 1, 'Midnights', '2022-10-21', 'https://fakecdn.com/album/midnights.jpg?token=alb1&expires=1725480000'),
+                                                                             (2, 2, 'Certified Lover Boy', '2021-09-03', 'https://fakecdn.com/album/clb.jpg?token=alb2&expires=1725480000'),
+                                                                             (3, 3, 'Map of the Soul: 7', '2020-02-21', 'https://fakecdn.com/album/mots7.jpg?token=alb3&expires=1725480000'),
+                                                                             (4, 4, 'Happier Than Ever', '2021-07-30', 'https://fakecdn.com/album/happier.jpg?token=alb4&expires=1725480000'),
+                                                                             (5, 5, 'After Hours', '2020-03-20', 'https://fakecdn.com/album/after_hours.jpg?token=alb5&expires=1725480000'),
+                                                                             (6, 6, 'Positions', '2020-10-30', 'https://fakecdn.com/album/positions.jpg?token=alb6&expires=1725480000'),
+                                                                             (7, 7, '=', '2021-10-29', 'https://fakecdn.com/album/equals.jpg?token=alb7&expires=1725480000'),
+                                                                             (8, 8, 'Future Nostalgia', '2020-03-27', 'https://fakecdn.com/album/future_nostalgia.jpg?token=alb8&expires=1725480000'),
+                                                                             (9, 9, 'YHLQMDLG', '2020-02-29', 'https://fakecdn.com/album/yhlqmdlg.jpg?token=alb9&expires=1725480000'),
+                                                                             (10, 10, 'SOUR', '2021-05-21', 'https://fakecdn.com/album/sour.jpg?token=alb10&expires=1725480000');
