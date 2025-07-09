4b16a71b21d6eac0906b98366a0a9c8ed0c5a85c
Yueh-Hsuan Wu <yuehhsuan.wu@student.mahidol.edu>
2025-07-09 15:34:40 +0700

    added concerts, songs, song stream count, playlists, and playlist songs

--- add_entries.sql
+++ add_entries.sql
@@ -35,6 +35,106 @@ INSERT INTO albums (album_id, artist_id, title, release_date, cover_url) VALUES
     (9, 9, 'YHLQMDLG', '2020-02-29', 'https://fakecdn.com/album/yhlqmdlg.jpg?token=alb9&expires=1725480000'),
     (10, 10, 'SOUR', '2021-05-21', 'https://fakecdn.com/album/sour.jpg?token=alb10&expires=1725480000');
 
+-- Filling in the rest of the table
+
+INSERT INTO concerts (concert_id, artist_id, title, country, city, venue, date, time, ticket_url, is_cancelled) VALUES
+    (1, 1, 'Live in Tokyo', 'Japan', 'Tokyo', 'Tokyo Dome', '2025-08-10', '19:00', 'https://tickets.com/tokyo', FALSE),
+    (2, 2, 'Acoustic Night', 'USA', 'New York', 'Madison Square Garden', '2025-09-05', '20:30', 'https://tickets.com/nyc', FALSE),
+    (3, 3, 'Cancelled Tour', 'UK', 'London', 'O2 Arena', '2025-07-20', '18:00', 'https://tickets.com/london', TRUE);
+
+INSERT INTO songs (song_id, title, artist_id, album_id, genre_id, duration, file_url, explicit) VALUES
+    -- Album 1
+    (1, 'Anti-Hero', 1, 1, 1, '00:03:20', 'https://cdn.com/antihero.mp3', FALSE),
+    (2, 'Lavender Haze', 1, 1, 1, '00:03:22', 'https://cdn.com/lavenderhaze.mp3', FALSE),
+
+    -- Album 2
+    (3, 'Girls Want Girls', 2, 2, 2, '00:03:41', 'https://cdn.com/gwg.mp3', TRUE),
+    (4, 'Champagne Poetry', 2, 2, 2, '00:05:36', 'https://cdn.com/champagnepoetry.mp3', FALSE),
+
+    -- Album 3
+    (5, 'Black Swan', 3, 3, 3, '00:03:18', 'https://cdn.com/blackswan.mp3', FALSE),
+    (6, 'ON', 3, 3, 3, '00:04:06', 'https://cdn.com/on.mp3', FALSE),
+
+    -- Album 4
+    (7, 'Your Power', 4, 4, 1, '00:04:05', 'https://cdn.com/yourpower.mp3', FALSE),
+    (8, 'Happier Than Ever', 4, 4, 1, '00:04:58', 'https://cdn.com/happierthanever.mp3', FALSE),
+
+    -- Album 5
+    (9, 'Blinding Lights', 5, 5, 3, '00:03:20', 'https://cdn.com/blindinglights.mp3', FALSE),
+    (10, 'Heartless', 5, 5, 3, '00:03:18', 'https://cdn.com/heartless.mp3', TRUE),
+
+    -- Album 6
+    (11, 'Positions', 6, 6, 1, '00:02:52', 'https://cdn.com/positions.mp3', FALSE),
+    (12, '34+35', 6, 6, 1, '00:02:54', 'https://cdn.com/34plus35.mp3', TRUE),
+
+    -- Album 7
+    (13, 'Shivers', 7, 7, 1, '00:03:27', 'https://cdn.com/shivers.mp3', FALSE),
+    (14, 'Bad Habits', 7, 7, 1, '00:03:50', 'https://cdn.com/badhabits.mp3', FALSE),
+
+    -- Album 8
+    (15, 'Levitating', 8, 8, 1, '00:03:23', 'https://cdn.com/levitating.mp3', FALSE),
+    (16, 'Physical', 8, 8, 1, '00:03:13', 'https://cdn.com/physical.mp3', FALSE),
+
+    -- Album 9
+    (17, 'Yo Perreo Sola', 9, 9, 5, '00:02:51', 'https://cdn.com/yoperreo.mp3', TRUE),
+    (18, 'La Difícil', 9, 9, 5, '00:03:22', 'https://cdn.com/ladificil.mp3', TRUE),
+
+    -- Album 10
+    (19, 'drivers license', 10, 10, 1, '00:04:02', 'https://cdn.com/driverslicense.mp3', FALSE),
+    (20, 'good 4 u', 10, 10, 1, '00:02:58', 'https://cdn.com/good4u.mp3', TRUE);
+
+INSERT INTO song_stream_counts (song_id, stream_count) VALUES
+    (1, 800000000),
+    (2, 720000000),
+    (3, 500000000),
+    (4, 450000000),
+    (5, 600000000),
+    (6, 550000000),
+    (7, 300000000),
+    (8, 250000000),
+    (9, 1600000000),
+    (10, 1000000000),
+    (11, 900000000),
+    (12, 850000000),
+    (13, 700000000),
+    (14, 690000000),
+    (15, 750000000),
+    (16, 500000000),
+    (17, 900000000),
+    (18, 870000000),
+    (19, 1000000000),
+    (20, 950000000);
+
+INSERT INTO playlists (playlist_id, title, user_id, is_public, created_at) VALUES
+    (1, 'Chill Pop', 5, TRUE, CURRENT_TIMESTAMP),
+    (2, 'Hype Hip-Hop', 6, TRUE, CURRENT_TIMESTAMP),
+    (3, 'K-Pop Collection', 8, FALSE, CURRENT_TIMESTAMP);
+
+-- Playlist 1: Chill Pop
+INSERT INTO playlist_songs (playlist_id, song_id, position) VALUES
+    (1, 1, 1),
+    (1, 2, 2),
+    (1, 11, 3),
+    (1, 15, 4),
+    (1, 19, 5);
+
+-- Playlist 2: Hype Hip-Hop
+INSERT INTO playlist_songs (playlist_id, song_id, position) VALUES
+    (2, 3, 1),
+    (2, 4, 2),
+    (2, 10, 3),
+    (2, 12, 4),
+    (2, 20, 5);
+
+-- Playlist 3: K-Pop Collection
+INSERT INTO playlist_songs (playlist_id, song_id, position) VALUES
+    (3, 5, 1),
+    (3, 6, 2),
+    (3, 9, 3),
+    (3, 16, 4),
+    (3, 18, 5);
+
+
 -- Adding users by register
 select register_user(
        'Peter',
